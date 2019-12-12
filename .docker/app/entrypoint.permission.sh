af447c06fc7e976158ce138c4818470176b67fd6
Marcelo V Gonçalves <cellobnu@Marcelos-MacBook-Pro.local>
2019-12-12 07:24:00 -0300

    Adiconado entrypoint modificando permissão.

--- .docker/app/entrypoint.permission.sh
+++ .docker/app/entrypoint.permission.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chown -R www-data:www-data /var/www
 find storage bootstrap/cache public -type f -exec chmod o+w {} \;
 find storage bootstrap/cache public -type d -exec chmod o+wx {} \;
 chmod o-w public/index.php
+chmod 777 -R storage/logs
